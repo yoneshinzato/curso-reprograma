60bfe94bfa3dac662550537ff35634aa1c7b7a34
yoneshinzato <chibariyo@gmail.com>
2018-11-21 20:14:54 -0200

    change

--- fullstack/node/aula-5/resources.js
+++ fullstack/node/aula-5/resources.js
@@ -30,26 +30,35 @@ app.post('/api/users', (req, res) => {
   res.send(newUser);
 });
 
-// app.put('caminho_para_atualizar_um_usuário', (req, res) => {
-//   // Procure o usuário a partir do id
-//   // Se não for encontrado, emita um erro que condiz e uma mensagem
-//   // Se for encontrado, faça a validação dos campos obrigatórios
-//   // Se estiver inválido, retorne um status que condiz e uma mensagem
-//   // Se for válido, atualize o usuário encontrado a partir do id com os parâmetros recebidos
-//   // Retorne o usuário atualizado
-// });
+app.put('caminho_para_atualizar_um_usuário', (req, res) => {
+  // Procure o usuário a partir do id
+  const foundUser = users.find(user => user.id === parseInt(req.params.id));
 
-// app.delete('/api/users/:id', (req, res) => {
-// });
+  // Se não for encontrado, emita um erro que condiz e uma mensagem
+  if (!foundUser) {
+    return res.status(404).send('Deu merda');
+  }
 
-// const schema = {
-//   title: Joi.string().min(3).required(),
-//   description: Joi.string().min(3).required(),
-// }
-// const validation = Joi.validate(params, schema);
-//
-// if (validation.error) {
-//   throw new PostitError(validation.error.details[0].message, 404);
-// }
+  // Se for encontrado, faça a validação dos campos obrigatórios
+  res.send(foundUser);
+  
+  
+  // Se estiver inválido, retorne um status que condiz e uma mensagem
+  // Se for válido, atualize o usuário encontrado a partir do id com os parâmetros recebidos
+  // Retorne o usuário atualizado
+});
+
+app.delete('/api/users/:id', (req, res) => {
+});
+
+const schema = {
+  title: Joi.string().min(3).required(),
+  description: Joi.string().min(3).required(),
+}
+const validation = Joi.validate(params, schema);
+
+if (validation.error) {
+  throw new PostitError(validation.error.details[0].message, 404);
+}
 
 app.listen(3000, () => console.log('Ouvindo na porta 3000...'));
